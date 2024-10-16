9db634676514743d61710c54ba786a52d4691b97
Charlieyhs <87665482+charlieyhs@users.noreply.github.com>
2024-10-15 22:37:02 -0500

    Update DatosPropietario.java

--- Taller_vehicular/src/com/co/unad/tallervehicular/DatosPropietario.java
+++ Taller_vehicular/src/com/co/unad/tallervehicular/DatosPropietario.java
@@ -5,12 +5,6 @@
  */
 package com.co.unad.tallervehicular;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
